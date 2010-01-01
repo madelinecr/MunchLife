96233ff93866d52ca404d4675c0899a9265a4ab3
Blaine Pace <blainepace@gmail.com>
2009-12-31 23:49:00 -0600

    Added logic so that level counter behaves as expected.

--- src/info/bpace/munchlife/MunchLifeActivity.java
+++ src/info/bpace/munchlife/MunchLifeActivity.java
@@ -2,14 +2,53 @@ package info.bpace.munchlife;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.view.View.OnClickListener;
 
 public class MunchLifeActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private int level = 1;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button up_button = (Button)findViewById(R.id.up_button);
+		up_button.setOnClickListener(mUpClickListener);
+		
+		Button down_button = (Button)findViewById(R.id.down_button);
+		down_button.setOnClickListener(mDownClickListener);
+	}
+
+	private OnClickListener mUpClickListener = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			TextView current_level = (TextView)findViewById(R.id.current_level);
+			if(level < 10)
+			{
+				level = level + 1;
+				current_level.setText("Level " + level);
+			}
+		}
+	};
+	
+	private OnClickListener mDownClickListener = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			TextView current_level = (TextView)findViewById(R.id.current_level);
+			if(level > 1)
+			{
+				level = level - 1;
+				current_level.setText("Level " + level);
+			}
+		}
+	};
 }
