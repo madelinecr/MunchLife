b65660897802103d98f280795025ffa1ff2fe1b3
Blaine Pace <blainepace@gmail.com>
2010-01-10 23:48:43 -0600

    Added basic readme, full GPL license text and GPL header to source file

---
+++ README.txt
@@ -0,0 +1,3 @@
+MunchLife is a very simple counter application for keeping track of your level while you're playing the card game Munchkin(tm). Released under the GPL version 3, see LICENSE.txt.
+
+http://www.worldofmunchkin.com/game/
--- src/info/bpace/munchlife/MunchLifeActivity.java
+++ src/info/bpace/munchlife/MunchLifeActivity.java
@@ -1,9 +1,30 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package info.bpace.munchlife;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.*;
-import android.view.*;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View.OnClickListener;
 
 public class MunchLifeActivity extends Activity
