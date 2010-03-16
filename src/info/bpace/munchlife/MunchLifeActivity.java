141ec5c07d4ad66e043e8e63471576b670016bfc
Blaine Pace <blainepace@gmail.com>
2010-03-16 16:54:03 -0700

    Removed unneeded dependency.

--- src/info/bpace/munchlife/MunchLifeActivity.java
+++ src/info/bpace/munchlife/MunchLifeActivity.java
@@ -30,7 +30,6 @@ import android.view.View.OnClickListener;
 
 import android.app.Dialog;
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 
